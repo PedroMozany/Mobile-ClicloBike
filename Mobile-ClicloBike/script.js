f7f53f0b30aa7ee357401dd650255ad8532ee9d4
77Nina <75265293+77Nina@users.noreply.github.com>
2021-06-20 21:11:38 -0300

    ajustes finos4

--- Mobile-ClicloBike/script.js
+++ Mobile-ClicloBike/script.js
@@ -1,5 +1,5 @@
-window.addEventListener("load", function() {
-    let name = document.getElementById('name');
+window.addEventListener("load", function () {
+    let name = document.getElementById("nameRegistro");
     let date = document.getElementById("data");
     let password = document.getElementById("senha");
     let cpassword = document.getElementById("confirmasenha");
@@ -7,17 +7,20 @@ window.addEventListener("load", function() {
     let ema = document.getElementById("email");
     let salvar = document.getElementById("salvar");
 
-    salvar.addEventListener("click", function() {
-      let input1=name.value
-      let input2=date.value
-      let input3=tel.value
-      let input4=ema.value
-      let input5=password.value
-      window.localStorage.setItem('nome', JSON.stringify(input1));
-      window.localStorage.setItem('data', JSON.stringify(input2));
-      window.localStorage.setItem('telefone', JSON.stringify(input3));
-      window.localStorage.setItem('email', JSON.stringify(input4));
-      window.localStorage.setItem('senha', JSON.stringify(input5));
+    salvar.addEventListener("click", function () {
+        let input1 = name.value;
+        let input2 = date.value;
+        let input3 = tel.value;
+        let input4 = ema.value;
+        let input5 = password.value;
+        window.localStorage.setItem("nome", JSON.stringify(input1));
+        window.localStorage.setItem("data", JSON.stringify(input2));
+        window.localStorage.setItem("telefone", JSON.stringify(input3));
+        window.localStorage.setItem("email", JSON.stringify(input4));
+        window.localStorage.setItem("senha", JSON.stringify(input5));
     });
-
 });
+
+function alertLogin() {
+    alert("Cadastro feito com sucesso!");
+}
